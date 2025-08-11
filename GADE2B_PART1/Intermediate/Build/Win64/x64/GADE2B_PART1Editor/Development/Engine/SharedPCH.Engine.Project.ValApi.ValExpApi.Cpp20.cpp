fa422eadf8fd3aaf90f2cf013936d153a0b6b0c0
xil555 <st10438975@vgconnect.edu.za>
2025-08-11 23:30:27 +0200

    Game arena commit

---
+++ GADE2B_PART1/Intermediate/Build/Win64/x64/GADE2B_PART1Editor/Development/Engine/SharedPCH.Engine.Project.ValApi.ValExpApi.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.42.34436
+#include "SharedPCH.Engine.Project.ValApi.ValExpApi.Cpp20.h"
---
+++ GADE2B_PART1/Source/GADE2B_PART1/GADE2B_PART1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GADE2B_PART1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GADE2B_PART1, "GADE2B_PART1" );
